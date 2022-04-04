6026eb864ea1f289fae534b7317722ae13e9a98a
OWL <tom.schuck@owlcry.de>
2022-04-04 11:44:11 +0200

    kinda working post backend

--- backend/requests.js
+++ backend/requests.js
@@ -12,10 +12,24 @@ const gets = (app, pool) => {
 
 const posts = (app, pool) => {
 app.post('/login', (req, res) => {
-let user_name = req.body.user;
-let password = req.body.password;
-console.log("User name = "+user_name+", password is "+password);
-res.end('yes');
+    let user_name = req.body.user;
+    let password = req.body.password;
+
+    let statement = "Select * from users where username = $1 and password = $2";
+    let values = [user_name, password];
+
+    pool.query(statement, values, (err, resp) => {
+        if (err) {
+            console.log(err.stack)
+            res.end("fail");
+        } else {
+            console.log(resp.rows[0])
+            if(resp.rows[0]){
+                res.end("ok");
+            } else res.end("fail");
+
+        }
+    })
 });
 }
 

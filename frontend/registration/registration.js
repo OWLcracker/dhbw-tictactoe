2eac3fbaed198c1e3c19d48c76800f5a175bed6e
Kilian <kiliankempf@gmx.de>
2022-04-05 16:53:22 +0200

    Password Bar

--- frontend/registration/registration.js
+++ frontend/registration/registration.js
@@ -1,18 +1,42 @@
 let usernameElem,
     passwordElem,
     passwordRepeatElem,
-    errorElem;
+    errorElem,
+    pwBar;
 
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|' +
+    '((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
 window.addEventListener("load", () => {
     usernameElem = document.getElementById("username");
+    pwBar = document.getElementById("password-bar");
     passwordElem = document.getElementById("password");
+    passwordElem.addEventListener("input", check);
     passwordRepeatElem = document.getElementById("passwordRepeat");
     errorElem = document.getElementById("error");
     document.getElementById("register").addEventListener("click", register);
 });
-
+function check(){
+    if(passwordElem.value===""){
+        pwBar.style.display = "none"
+    }
+    else if(strongPassword.test(passwordElem.value)){
+        pwBar.style.display = "block"
+        pwBar.style.backgroundColor = "green";
+    }
+    else if (mediumPassword.test(passwordElem.value)){
+        pwBar.style.display = "block"
+        pwBar.style.backgroundColor  = "orange";
+    }
+    else{
+        pwBar.style.display = "block"
+        pwBar.style.backgroundColor  = "red";
+    }
+}
 async function register() {
     let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+    let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|' +
+                                            '((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
     // Check input
     if (usernameElem.value === "" || passwordElem.value === "" || passwordRepeatElem.value === ""
         || passwordElem.value !== passwordRepeatElem.value) {
@@ -31,7 +55,7 @@ async function register() {
 
         let userJson = JSON.parse('{ "user": "' + username.value + '", "password": "' + password.value + '" }');
         let isError = false;
-        let response = await fetch('http://localhost:3000/login',
+        let response = await fetch('http://localhost:3000/register',
             {
                 headers: {
                     'Content-Type': 'application/json'
@@ -42,13 +66,13 @@ async function register() {
             console.error('Error:', error);
             isError = true;
         });
-        if (isError) {
+        if (isError||response.status===400) {
             errorElem.innerHTML = 'Error. Please try again later.';
             errorElem.style.display = "block";
         }
         else if (response.status===200) {
             window.open("../menu/menu.html", "_self");
-        } else {
+        } else if (response.status===406) {
             errorElem.innerHTML = 'Error. User already exists.'
             errorElem.style.display = "block";
         }

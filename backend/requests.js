f77d598ede51fbba62fada523dfd34610fd29b74
OWL <tom.schuck@owlcry.de>
2022-04-05 15:44:20 +0200

    added user_exists query backend

--- backend/requests.js
+++ backend/requests.js
@@ -1,7 +1,24 @@
+const {response} = require("express");
+
+const user_exists = async (user, pool) => {
+    let statement = "Select * from users where username = $1";
+    let values = [user];
+    let resp;
+    let error;
+    try {
+        resp = await pool.query(statement, values)
+    } catch (err) {
+        error = err;
+    }
+    return {
+        resp,
+        error
+    }
+}
 const gets = (app, pool) => {
     app.get('/loginuser', (req, res, next) => {
         console.log("Login User :");
-        pool.query('Select * from Users')
+        pool.query('Select * from users')
             .then(Users => {
                 console.log(Users);
                 res.send(Users.rows);
@@ -15,21 +32,18 @@ app.post('/login', (req, res) => {
     let user_name = req.body.user;
     let password = req.body.password;
 
-    let statement = "Select * from users where username = $1 and password = $2";
-    let values = [user_name, password];
-
-    pool.query(statement, values, (err, resp) => {
-        if (err) {
-            console.log(err.stack)
-            res.end("fail");
-        } else {
-            console.log(resp.rows[0])
-            if(resp.rows[0]){
-                res.end("ok");
-            } else res.end("fail");
-
+    user_exists(user_name, pool).then((response)=>{
+        if(response.error){
+            res.status(400).send();
+        }else{
+            if(response.resp.rows[0]){
+                res.send(response.resp.rows[0]);
+            }else{
+                res.status(404).send();
+            }
         }
-    })
+    });
+
 });
 }
 

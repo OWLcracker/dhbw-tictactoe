878573b3e36bc3765cf87dde73deb0603f07a256
Kilian <kiliankempf@gmx.de>
2022-04-06 18:14:45 +0200

    Password Bar

--- frontend/registration/registration.js
+++ frontend/registration/registration.js
@@ -6,7 +6,8 @@ let usernameElem,
 
 let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
 let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|' +
-    '((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
+    '((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))|' +
+    '((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))');
 window.addEventListener("load", () => {
     usernameElem = document.getElementById("username");
     pwBar = document.getElementById("password-bar");

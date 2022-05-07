640fe2024208f679839449b64de2146845d306c1
OWL <tom.schuck@owlcry.de>
2022-05-07 13:22:54 +0200

    further login features

--- backend/requests.js
+++ backend/requests.js
@@ -3,8 +3,7 @@ const {response} = require("express");
 const user_exists = async (user, pool) => {
     let statement = "Select * from users where username = $1";
     let values = [user];
-    let resp;
-    let error;
+    let resp, error;
     try {
         resp = await pool.query(statement, values)
     } catch (err) {
@@ -15,12 +14,36 @@ const user_exists = async (user, pool) => {
         error
     }
 }
+const user_authenticate = async (user, password, pool) => {
+    let statement = "Select * from users where username = $1 and password = $2";
+    let values = [user, password];
+    try {
+        resp = await pool.query(statement, values)
+        return !!resp.rows[0];
+    } catch (err) {
+        return false;
+    }
+}
+
+const getSession = async (user, pool) => {
+    let statement = "Select * from sessions as sess, users where sess.user_id = users.user_id and sess.user_id = $1";
+    let values = [user];
+    try {
+        resp = await pool.query(statement, values)
+        if(resp.rows[0]){
+           // if(resp.rows[0].creationDate > now?); // hier weiter machen
+        }
+    } catch (err) {
+        return false;
+    }
+}
+
 const gets = (app, pool) => {
     app.get('/loginuser', (req, res, next) => {
         console.log("Login User :");
         pool.query('Select * from users')
             .then(Users => {
-                console.log(Users);
+                console.log(Users.rows[0].user_id);
                 res.send(Users.rows);
 
             })
@@ -32,15 +55,12 @@ app.post('/login', (req, res) => {
     let user_name = req.body.user;
     let password = req.body.password;
 
-    user_exists(user_name, pool).then((response)=>{
-        if(response.error){
-            res.status(400).send();
+    user_authenticate(user_name, password, pool).then((bool)=>{
+        if(bool){
+            //generate session key and set it in db
+            res.send("generated session key");
         }else{
-            if(response.resp.rows[0]){
-                res.send(response.resp.rows[0]);
-            }else{
-                res.status(404).send();
-            }
+            res.status(404).send();
         }
     });
 
